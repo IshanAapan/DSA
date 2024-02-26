c4bc8fb43c9a77b261ddc658e39f4164a43a0d72
IshanAapan <80051311+IshanAapan@users.noreply.github.com>
2024-02-26 17:15:03 +0530

    Update slidingWindow_maxSum of an subArray.cpp

--- slidingWindow_maxSum of an subArray.cpp
+++ slidingWindow_maxSum of an subArray.cpp
@@ -12,7 +12,7 @@ int main(){
     int ans = INT_MIN;
     for(int i=0;i<n;i++){
         sum+=v[i];                // include right part element
-        if(i-start+1==k){
+        if(i-start+1==k){        //checking the window size
             ans=max(ans,sum);
             sum-=v[start];        //exclude left part element
             start++;
