3bf330c8bbd7e0841a02cfe78e46b192f676e450
IshanAapan <80051311+IshanAapan@users.noreply.github.com>
2024-02-27 16:35:30 +0530

    Create slidingWindow-4.cpp

---
+++ slidingWindow-4.cpp
@@ -0,0 +1,46 @@
+// find the range of max sum from an subArray nearest to start
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void maxSum_of_subArray(vector <int>v,int n,int k){
+     int sum = 0,start = 0, ans = INT_MIN;
+    for(int i=0;i<n;i++){
+        sum+=v[i];
+        if((i-start+1)==k){
+            ans = max(ans,sum);
+            sum-=v[start];
+            start++;
+        }
+    }
+    cout<<"Max:->"<<ans;
+}
+
+void range_maxSum_subArray_nearest_to_start(vector <int>v,int n,int k){
+    int sum = 0,start  = 0,prevSum=-1;
+    pair<int,int>p;
+    for(int i=0;i<n;i++){
+        sum+=v[i];
+        if((i-start+1)==k){
+            if(prevSum<sum){
+                prevSum=sum;
+                p.first=start,p.second=i;
+            }
+            sum-=v[start];
+            start++;
+        }
+    }
+    cout<<"count"<<p.first<<"\t"<<p.second;
+}
+
+int main() {
+    int n,k;
+    cin>>n>>k;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    maxSum_of_subArray(v,n, k);
+    range_maxSum_subArray_nearest_to_start(v, n,k);
+    return 0;
+}
